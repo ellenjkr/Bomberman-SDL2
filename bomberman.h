999da43d52b27424ed150ea72bb9eaeafa215e50
Ellen Junker <ellenjunker2000@gmail.com>
2019-10-15 02:13:58 -0300

    Update bomberman.h

--- bomberman.h
+++ bomberman.h
@@ -15,12 +15,21 @@ struct Posicao{
     SDL_Rect destino;
 };
 
+struct Bomba{
+    SDL_Texture *imagem;
+    Posicao posicao;
+    int quantidade = 1;
+    bool plantada = false;
+    int valorMapa;
+};
+
 struct Personagem{
     SDL_Texture *imagem;
     Posicao posicao;
-    int bombas = 1;
-    bool bomba = false;
+    Bomba bomba;
     int vida = 3;
+    int dano = 0;
+    int valorMapa;
 };
 
 struct Componentes{
@@ -33,28 +42,29 @@ enum Itens{
     PAREDE,
     CAIXA,
     PLAYER1,
-    BOT,
-    BOMBA,
+    PLAYER2,
+    BOMBA1,
+    BOMBA2,
     FOGO
 };
 
 SDL_Texture* carregaImagem(const char *imagem, SDL_Renderer *renderizador);
 
-void imprimeMapa(Componentes fogo, Componentes bomba, Componentes caixa, Componentes parede, Componentes fundo, Personagem player, Personagem bot, SDL_Renderer *renderizador, int mapa[13][15], int mapaBomba[13][15]);
+void imprimeMapa(Componentes fogo, Componentes caixa, Componentes parede, Componentes fundo, Personagem player, Personagem bot, SDL_Renderer *renderizador, int mapa[13][15], int mapaBomba[13][15]);
 
-void validaMovimento(int mapa[13][15], int indiceX, int indiceY);
+void validaMovimento(int mapa[13][15], int indiceX, int indiceY, int player);
 
 void plantaBomba(Personagem &player, int mapaBomba[13][15], long int &tempoBomba, int indiceX, int indiceY);
 
-void movimentaPersonagem(bool &termina, long int &tempoBomba, Personagem &player, int mapa[13][15], int mapaBomba[13][15]);
+void movimentaPlayers(bool &termina, long int &tempoBomba, Personagem &player, long int &tempoBomba2, Personagem &player2, int mapa[13][15], int mapaBomba[13][15]);
 
-void movimentaBot(int mapa[13][15], Personagem &bot, int mapaBomba[13][15]);
+void movimentaBot(int mapa[13][15], Personagem &bot);
 
-void bombaAnimacao(Componentes &bomba, long int tempoInicio, long int tempoAtual);
+void bombaAnimacao(Personagem &player, long int tempoInicio, long int tempoAtual);
 
 void validaExplosao(int mapa[13][15], int mapaBomba[13][15], int indiceX, int indiceY, int adicionalX, int adicionalY);
 
-bool explodeBomba(long int tempoInicio, int mapa[13][15], int mapaBomba[13][15], long int tempoAtual);
+void explodeBomba(Personagem player, long int tempoInicio, long int tempoAtual, int mapa[13][15], int mapaBomba[13][15]);
 
 void verificaVidaPlayer(int mapa[13][15], int mapaBomba[13][15], Personagem &player);
 
