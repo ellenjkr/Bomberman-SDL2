e0cf77e7a6ec40d8d66ae698e07d6b8fbd94d30a
Ellen Junker <ellenjunker2000@gmail.com>
2020-07-27 17:03:33 -0300

    Update bomberman.h

--- bomberman.h
+++ bomberman.h
@@ -36,6 +36,13 @@ struct Personagem{
     int fogoMapa;
 };
 
+struct Bot{
+    Personagem personagem;
+    int direcao;
+    int quantidadePassos = 0;
+    int passosAndados = 0;
+};
+
 struct Componentes{
     SDL_Texture *imagem;
     Posicao posicao;
@@ -53,13 +60,36 @@ enum Itens{
     FOGO2
 };
 
+enum Jogo{
+    SINGLEPLAYER,
+    MULTIPLAYER
+};
+
+enum Menu{
+    INSTRUCOES,
+    JOGAR,
+    SAIR
+};
+
 SDL_Texture* carregaImagem(const char *imagem, SDL_Renderer *renderizador);
 
 Personagem criaPlayer1(Personagem player1, SDL_Renderer *renderizador, SDL_Texture *personagens);
 
 Personagem criaPlayer2(Personagem player2, SDL_Renderer *renderizador, SDL_Texture *personagens);
 
-void criaComponentes(SDL_Renderer *renderizador, Componentes &fundo, Componentes &caixa, Componentes &parede, Componentes &fogo, Componentes &botaoHome, Componentes &botaoRestart);
+void criaComponentes(SDL_Renderer *renderizador, Componentes &menuPrincipal, Componentes &instrucoes, Componentes &menuJogar, Componentes &fundo, Componentes &caixa, Componentes &parede, Componentes &fogo, Componentes &botaoHome, Componentes &botaoRestart);
+
+void criaFim(SDL_Renderer *renderizador, Componentes &empate, Componentes &player1Vence, Componentes &player2Vence);
+
+int escolheMenuPrincipal(Componentes &menu, SDL_Cursor *cursor, bool &termina);
+
+void carregaInstrucoes(Componentes &instrucoes, bool &abreInstrucoes, bool &abreMenuPrincipal, SDL_Cursor *cursor, bool &termina);
+
+void escolheMenuJogar(Componentes &menuJogar, bool &abreMenuPrincipal, bool &abreMenuJogar, bool &jogaJogo, int &jogo, SDL_Cursor *cursor, bool &termina);
+
+void resetMultiplayer(int mapa[13][15], int mapaBomba[13][15], Personagem &player1, Personagem &player2);
+
+void resetSinglePlayer(int mapaSinglePlayer[13][15], int mapaBomb[13][15], Personagem &player1, Bot &bot);
 
 void imprimeMapa(Componentes fogo, Componentes caixa, Componentes parede, Componentes fundo, Personagem player, Personagem bot, SDL_Renderer *renderizador, int mapa[13][15], int mapaBomba[13][15]);
 
@@ -67,9 +97,11 @@ void validaMovimento(int mapa[13][15], int indiceX, int indiceY, int player);
 
 void plantaBomba(Personagem &player, int mapaBomba[13][15], int indiceX, int indiceY);
 
-void movimentaPlayers(bool &termina, Personagem &player, Personagem &player2, int mapa[13][15], int mapaBomba[13][15]);
+void movimentaPlayers(bool &termina, Personagem &player, Personagem &player2, int mapa[13][15], int mapaBomba[13][15], bool &abreMenuPrincipal, bool &jogaJogo, SDL_Cursor *cursor);
 
-void movimentaBot(int mapa[13][15], Personagem &bot);
+void movimentaPlayer1(bool &termina, Personagem &player, Bot &bot, int mapa[13][15], int mapaBomba[13][15], bool &abreMenuPrincipal, bool &jogaJogo, SDL_Cursor *cursor);
+
+void movimentaBot(int mapa[13][15], Bot &bot, int frameCounter);
 
 void bombaAnimacao(Personagem &player, long int tempoAtual);
 
@@ -91,7 +123,9 @@ void explodeBomba1(Personagem &playerPrincipal, Personagem &playerSecundario, in
 
 void explodeBomba2(Personagem &playerPrincipal, Personagem &playerSecundario, int mapa[13][15], int mapaBomba[13][15], long int tempoAtual);
 
+bool verificaFimDeJogo(Personagem &player1, Personagem &player2);
 
+void imprimeFimDeJogo(Personagem &player1, Personagem &player2, Componentes &empate, Componentes &player1Vence, Componentes &player2Vence, SDL_Renderer *renderizador, SDL_Cursor *cursor, bool &abreMenuPrincipal, bool &jogaJogo, bool &termina);
 
 
 #endif // BOMBERMAN_H
